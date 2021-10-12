9181c1eda54bb76289c6478f37bc6a6f05d99552
rugg07 <86529890+rugg07@users.noreply.github.com>
2021-10-12 17:44:29 +0400

    Update squares-of-a-sorted-array.cpp

--- squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,3 +1,4 @@
+//Brute force approach
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
@@ -8,5 +9,32 @@ public:
         
         sort(nums.begin(), nums.end()); //inbuilt sorting for vector arrays (ALWAYS use .begin() and .end())
         return nums; //return pointer of the array
+    }
+};
+
+//Optimized - (Two Pointer Approach)
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int start=0; //pointer to first index 
+        int end=nums.size()-1; //pointer to last index
+        int pointer=nums.size()-1; //pointer for last index of resultant array
+        vector<int> result =nums; //make 
+        while(start<=end)
+        {
+            if(abs(nums[start])>abs(nums[end])) //find the bigger number by doing the absolute since square of a number is always +ve
+               {
+                   result[pointer]=nums[start]*nums[start];//square and put value to the end
+                   start++; //increase start position
+                   pointer--; //since it points to last index we decrement
+               }
+               else
+               {
+                   result[pointer]=nums[end]*nums[end];
+                   end--; //decrement end to previous element
+                   pointer--;
+               }
+        }
+              return result; //pointer to resultant array
     }
 };
