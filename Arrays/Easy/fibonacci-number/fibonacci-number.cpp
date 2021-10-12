b9097a6545f1f4eb47b6bbb375d0dec40d8e38da
rugg07 <86529890+rugg07@users.noreply.github.com>
2021-10-13 00:33:47 +0400

    Update fibonacci-number.cpp

--- Arrays/Easy/fibonacci-number/fibonacci-number.cpp
+++ Arrays/Easy/fibonacci-number/fibonacci-number.cpp
@@ -4,13 +4,16 @@ public:
       
         if(n==0)
        {
-           return 0;
+           return 0; //use 0 
        }
         if(n==1)
         {
-            return 1;
+            return 1; //use 1
         }
         
-            return fib(n-1)+fib(n-2);
+            return fib(n-1)+fib(n-2); //instead of taking another element equating to this equation and returning we directly return the value in this way
     }
-};
+}; 
+
+//Time Complexity - O(2^N)
+//Space Complexity - O(N) given the function call stack size
