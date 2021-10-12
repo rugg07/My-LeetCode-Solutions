5c7a530e76651520247ed6e4a85076d3311cfe91
rugg07 <86529890+rugg07@users.noreply.github.com>
2021-10-13 00:36:29 +0400

    Update remove-duplicates-from-sorted-array.cpp

--- Arrays/Easy/remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ Arrays/Easy/remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -2,18 +2,22 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int count=0;
-        for(int i=1; i<nums.size(); i++)
+        for(int i=1; i<nums.size(); i++) //start from 2nd element (1st index)
         {
-            if(nums[i]==nums[i-1])
+            if(nums[i]==nums[i-1]) //if current index element = previous index element
             {
-                count++;
+                count++; //increase count for the element
             }
             else
             {
-                nums[i-count]=nums[i];
+                nums[i-count]=nums[i]; //otherwise add nums[i] value to nums[i-count]
             }
         }
        
-        return nums.size()-count;
+        return nums.size()-count; //return the the first k values since it goes from 0 to nums.size()-count 
     }
 };
+
+//why nums[i-count]? because since its sorted all duplicates will remain together so after going through one element it's count increases.Therefore, i-count gives the next index where no duplicate could be possible 
+
+//0,0,1,1 =>0,1
