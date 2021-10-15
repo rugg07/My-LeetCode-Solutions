f66adfa1bd37c327b54713c674784ecdde300729
rugg07 <86529890+rugg07@users.noreply.github.com>
2021-10-15 13:05:14 +0400

    Update climbing-stairs.cpp

--- Dynamic Programming/Easy/climbing-stairs/climbing-stairs.cpp
+++ Dynamic Programming/Easy/climbing-stairs/climbing-stairs.cpp
@@ -3,13 +3,13 @@ public:
     int climbStairs(int n) 
     {   //It is Fibonacci sequence where numbers start from 1 instead of 0,1(first 2 numbers of fibonacci series) so we get 1,2,3,5,8,13,... for steps 1,2,3,4,5,6,...
         
-        if(n==1) //if only one step needs to be taken then by theory we can only take one step
+          if(n==1) //if only one step needs to be taken then by theory we can only take one step
             return 1;
         
         int a=1; //step 1
         int b=1; //step 2
         int ans;
-        for(int i=1;i<n;i++) 
+        for(int i=1;i<n;i++) //calculating total number of steps to be taken until we reach nth step
         {
             ans=a+b;
             a=b;
@@ -17,7 +17,6 @@ public:
         }
         return ans;
         
-        //we don't check if n==0 because given constraint is n has to be between 1 to 45
         
     }
 };
