1284310a5bab574da03774dcb7942e3111f7d70e
rugg07 <86529890+rugg07@users.noreply.github.com>
2021-11-14 11:04:43 +0400

    Time: 20 ms (87.66%), Space: 24 MB (55.63%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.cpp
+++ product-of-array-except-self/product-of-array-except-self.cpp
@@ -3,44 +3,31 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
         
-        //Make 3 loops for 3 arrays
-        //1.product on the left side of nums[i]
-        //2.product on the right side of nums[i]
-        //3.final product
-        
-        //1. Left side product array
-        int left_product[n];
-        left_product[0]=1; //first element will be 1 else it will take random value
-        
-        //2. Right side product array
-        int right_product[n];
-        right_product[n-1]=1; //last element will be 1 else it will take random value
-        
-
+//final product array
 //return type is vector<int> we initialize entire array of size n with 1 else it doesnt give output
-         vector<int> final_product(n,1);
+        vector<int> final_product(n,1);
+        final_product[0]=1; //first element will be 1 else it will take random value
         
+           
         //Products of left side
         for(int i=1; i<n; i++)
         {
             //we start from 1st index and multiply with value of last index for both arrays
-            left_product[i]=left_product[i-1]*nums[i-1];
+            final_product[i]=final_product[i-1]*nums[i-1];
         }
+        
+        
+        //Right side product in final_product array
+        int right_product=1; //last element will be 1 else it will take random value
         
         //Products of right side
-        for(int i=n-2; i>=0; i--)
+        for(int i=n-1; i>=0; i--)
         {
-        //we start from 2nd last index and multiply with value of the succeding index of both array
-            right_product[i]=right_product[i+1]*nums[i+1];
+            final_product[i]=final_product[i]*right_product;
+            right_product*=nums[i]; 
+            //right_product increases with each element going from right to left
         }
         
-        //Product of final array
-        for(int i=0;i<n;i++)
-        {
-            
-        //Multiply on both sides
-            final_product[i] = left_product[i]*right_product[i];
-        }
        return final_product;
     }
 };
