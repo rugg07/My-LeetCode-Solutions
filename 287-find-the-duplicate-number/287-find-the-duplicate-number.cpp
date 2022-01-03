bec8db0553209945a93643f87789a53678a7017e
rugg07 <86529890+rugg07@users.noreply.github.com>
2022-01-03 23:47:24 +0400

    Time: 153 ms (29.41%), Space: 61.3 MB (58.08%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -2,10 +2,40 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         
+ //Optimal Approach: Linked List Cycle Method
+        
+        //initialize both pointers to first element
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        //keep incrementing slow by one step and fast by 2 steps. Do till slow and fast don't meet.
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow!=fast);
+        
+        //Now make fast to first element again and increment slow and fast by one step now
+        
+        fast = nums[0];
+        
+        while(slow!=fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        //since nums and fast met and both have the value of duplicate element
+        return slow;
+    }
+};
+
+//TC:O(n) traversing only once
+//SC:O(1) as constant space is only used.
 
  
 /*----------------------------------------------------------------------------------------------*/
 
+/*
 //Better Approach:
   int n = nums.size();
         
@@ -32,5 +62,5 @@ public:
 
 //TC: O(n)+O(n) [traverse org arr + traverse new arr]
 //SC: O(n) [new arr]
-
+*/
         
