16f42408bb60af85b6d7c696e1e4fb911afbaf5d
rugg07 <86529890+rugg07@users.noreply.github.com>
2022-01-12 18:41:40 +0400

    Time: 0 ms (100.00%), Space: 6 MB (68.21%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,7 +1,35 @@
 class Solution {
 public:
+   //OPTIMAL: (COMBINATIONS)
+    int uniquePaths(int m, int n) {
+        
+//we will always take RIGHT(m-1) steps and BOTTOM(n-1) steps to final index. TotalSteps=(m-1)+(n-1)
+//total_steps acts as n (numerator) of nCr
+       int total_steps=m+n-2;
+
+//to find number of unique paths we can either take m-1 or n-1, since it always requires those many number of steps to reach final destination
+        int r=m-1;
+        
+//total unique path would be m+n-2 C r  
+        double res=1;
+        
+//we take only the last r number ele from total_steps. eg:from 10C3 we take 10*9*8/3*2*1(last3ele)
+//here i acts as r! so we keep dividing by i, eg: 3! => 1*2*3
+        for(int i=1;i<=r;i++)
+        {
+            res= res*(total_steps-r+i)/i;
+        }
+        return res;
+    }
+};
+//TC: O(m-1) OR O(n-1) [depends on if you calc Combinations with m-1 or n-1]
+//SC: O(1) 
+
+/*-------------------------------------------------------------------------------------------------
+class Solution {
+public:
     int countPaths(int i, int j, int m, int n, vector<vector<int>> &dp){
-         //Better: Recursion + DP
+         //BETTER: [RECURSION + DP]
         
         //if it hits final index we return 1
         if(i==(m-1) || j==(n-1))
@@ -26,13 +54,14 @@ public:
         return countPaths(i,j,m,n,dp);
     }
 };
-//TC:O(n*m) [At max there will be n*m states, after that there wont be any further recursive calls]
-//SC:O(n*m) [initializing a dynamic array of m*n]
-
+//TC:O(m*n) [At max there will be n*m states, after that there wont be any further recursive calls]
+//SC:O(m*n)+O(m*n) [initializing a dynamic array of m*n + Recursive space of m*n]
+  => O(m*n) aprox.
+*/
 /*-------------------------------------------------------------------------------------------------class Solution {
 public:
     int countPaths(int i, int j, int m, int n){
-         //Brute Force: use Recursion
+         //BRUTE FORCE: [RECURSION]
         
         //if it hits final index we return 1
         if(i==(m-1) || j==(n-1))
