f9f6108de5f87b672af7c6988c3dbd0aa021cb05
rugg07 <86529890+rugg07@users.noreply.github.com>
2022-01-19 20:20:24 +0400

    Time: 4 ms (96.65%), Space: 8.2 MB (77.61%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,6 +1,53 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        //OPTIMAL: to reduce movement of left pointer  
+        
+        //make frequency array for 256 ASCII charecters and initialise all positions with -1 value
+        //string can have only 256 different charecters
+        vector<int> unique(256,-1);
+        
+        int n=s.size();
+        
+        if(n==0) //no charecters present
+            return 0;
+        
+        int length=0; //current length without repetition in substring
+        int max_length=0; //maximum possible length without repetition in substring
+        
+        //both pointers start from first ele
+        int left=0, right=0;
+        
+        while(right<n)
+        {
+            //if ele is present the value will be of the index in string.
+            if(unique[s[right]]!=-1)
+            {
+             //We do max so left pointer can jump one ele after the previous repeating ele's pos.
+                  left = max(unique[s[right]]+1,left); 
+            }
+            
+//update the position of the ele from the string in the freq array with the right pointer index.   
+            unique[s[right]] = right; 
+            
+            //find max length
+            length = right-left+1;
+            max_length = max(max_length, length);
+            
+            right++; //move right pointer to next ele      
+        }
+        return max_length; 
+    }
+};
+//TC: O(n)+O(1)=>O(n) [right pointer moves n spaces + lookup in freq arr]
+//SC: O(256)=>O(1) [set can contain at max all the 256 ASCII charecters. It won't increase more with the size of data.]
+
+
+/*------------------------------------------------------------------------------------------------
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
         //BETTER: using 2 pointers and hashSet
         
         //use unordered_set for c++
@@ -40,4 +87,5 @@ public:
     }
 };
 //TC: O(n)+O(n)=>O(2n) [both left and right pointers need to move n steps assuming last two character are repeating]
-//SC: O(n) [hashSet]
+//SC: O(256)=>O(1) [set can contain at max all the 256 ASCII charecters. It won't increase more with the size of data.]
+*/
