3019e782389d671337e80722783af26dd33fddd9
rugg07 <86529890+rugg07@users.noreply.github.com>
2022-01-27 21:58:10 +0400

    Time: 27 ms (5.50%), Space: 14.6 MB (98.48%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -8,13 +8,40 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        //similar to adding two ele in linked list
+       //OPTIMAL: inplace (not creating external/new nodes)
+        
+        ListNode *start = new ListNode();               //false head
+      ListNode *tmp = start;                          //working node 
+      
+      while(list1 && list2){                                //if we have two lists
+        if(list1->val < list2->val){tmp->next = list1; list1 = list1->next;}
+        else {tmp->next = list2; list2 = list2->next;}
+        tmp = tmp->next;
+      }
+      if(list1) tmp->next = list1;     //if we have only list1 or we have remain as list1
+      if(list2) tmp->next = list2;     //if we have only list2 or we have remain as list2
+      return start->next;            //return real head
+    }
+};
+
+/*
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        //BRUTE FORCE: similar to adding two ele in linked list
         
         ListNode* dummy = new ListNode(); //to make new head of new list
         ListNode* temp = dummy;//points to the new head
+        
+        if(l1==NULL) //list 1 empty
+            return list2;
+     
+        if(l2==NULL) //list 2 empty
+            return list1;
         
         //we check if curr ele is null not ->next!=NULL
         //do && so it breaks loop only when both lists go to null
@@ -48,5 +75,6 @@ public:
         
     }
 };
-//TC: O(max(n1,n2)) [max of (size of list1, size of list2)]
-//SC: O(n) [creating new list of n ele]
+//TC: O(n1+n2) [traverse list1 and list 2]
+//SC: O(n1+n2) => O(n) [creating new list of n ele]
+*/
