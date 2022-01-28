cbf6b2147f19b0a60286fd859036de7dc176c6b0
rugg07 <86529890+rugg07@users.noreply.github.com>
2022-01-28 20:59:26 +0400

    Time: 78 ms (28.73%), Space: 14.4 MB (88.73%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -12,6 +12,40 @@ eg: A=[4,1,8,4,5], B=[5,6,1,8,4,5] (both lists have 8,4,5 so ele 8 is intersecti
 
 class Solution {
 public:
+	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+		//OPTIMAL: Two-Pointers Solution
+        
+        //make new pointers and point to head of each list
+        ListNode *ptr1 = headA;
+		ListNode *ptr2 = headB;
+		
+        //loop runs over & over the lists till pointers point to same node
+        while(ptr1 != ptr2) 
+        {
+			if(ptr1 == NULL)
+				ptr1 = headB; //point to head of the other list (listB)
+
+			else
+				ptr1 = ptr1 -> next;
+		
+			if(ptr2 == NULL)
+				ptr2 = headA; //point to head of the other list (listA)
+			
+			else
+				ptr2 = ptr2 -> next;
+		}
+        
+        //if there exists an intersected node it returns that ele it returns NULL 
+        //since both pointers would break the loop on pointing to NULL
+		return ptr1; 
+	}
+};
+//TC: O(n)+O(n) = O(2n) [traverse listA + traverse listB]
+//SC: O(1)
+
+/*----------------------------------------------------------------------------------------
+class Solution {
+public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         //OPTIMAL: using length difference
         
@@ -64,11 +98,12 @@ public:
 			headA = headA -> next;
 			headB = headB -> next;
 		}
-		return NULL;
+		return NULL; //when they dont intersect
     }
 };
-//TC: O(n) [traverse n ele of both lists]
+//TC: O(m)+O(m-n)+O(n) = O(2m) [traverse: longer list + difference + shorter list]
 //SC: O(1)
+*/
 
 /*---------------------------------------------------------------------------------------
 class Solution {
