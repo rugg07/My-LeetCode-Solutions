0009a5e86bb693124853609a799c04bb980b14ef
rugg07 <86529890+rugg07@users.noreply.github.com>
2022-01-29 20:19:20 +0400

    Time: 13 ms (48.01%), Space: 8 MB (57.57%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -7,16 +7,34 @@
  * };
  */
 
-/*class Solution {
+class Solution {
 public:
     bool hasCycle(ListNode *head) {
         //OPTIMAL: Two-pointer approach (slow & fast)
         
-       
+        if(head==NULL) //no ele in the list
+            return false;
+        
+        //make two pointers that point to the head node
+        ListNode *slow = head;
+        ListNode *fast = head;
+        
+        //traverse until both dont point to NULL 
+        while(fast!=NULL && slow!=NULL && fast->next!=NULL)
+        {
+            slow=slow->next; //move by 1 step
+            fast=fast->next->next; //move by 2 steps
+            
+            if(fast==slow) //cycle is present
+                return true;
+        }
+        return false; //no cycle was found OR slow and fast point to NULL
     }
 };
-*/
+//TC: O(n) [traverse through the list]
+//SC: O(1)
 
+/*----------------------------------------------------------------------------------------
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
@@ -28,19 +46,20 @@ public:
         
         unordered_map<ListNode*, int> map; //store the node and count
         
-        ListNode * curr=head; //pointer for the list
+        ListNode * dummy = head; //pointer for the list
         
-        while(curr!=NULL)
+        while(dummy!=NULL)
         {
-            map[curr]++; //update count of the node in hashmap
+            map[dummy]++; //update count of the node in hashmap
             
-            if(map[curr]>1) //if count>1 means node is repeating
+            if(map[dummy]>1) //if count>1 means node is repeating
                 return true;
             
-            curr=curr->next;//go to next node
+            dummy = dummy->next;//go to next node
         }
         return false;
     }
 };
 //TC: O(n) [traverse through list]
 //SC: O(n) [hashMap to store all n ele]
+*/
