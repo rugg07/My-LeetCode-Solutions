64c4a93b019bd248895c65bd368961b82ce3897c
rugg07 <86529890+rugg07@users.noreply.github.com>
2022-02-01 20:55:32 +0400

    Time: 14 ms (41.96%), Space: 7.6 MB (75.86%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -6,24 +6,40 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-/*class Solution {
+
+class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-      ListNode *slow=head, *fast=head;
-        while(slow!=NULL)
-        {
-            if(slow==fast)
-                return slow->next;
-            else
-            {
-                slow=slow->next;
-                fast=fast->next->next;
-            }
-        }
+        
+         //no ele in the list OR only 1 ele in list so no cycle is possible
+        if(head==NULL || head->next==NULL) 
+            return NULL;
+        
+        ListNode *slow=head, *fast=head, *entry=head; 
+        
+       while(fast!=NULL && fast->next!=NULL)
+       {
+           //move slow & fast to find its collision in the cycle
+           slow=slow->next;
+           fast=fast->next->next;
+           
+        //once collided, use entry starting from head and slow to find first node in cycle
+           if(slow==fast)
+           {
+               while(slow!=entry)
+               {
+                   slow=slow->next;
+                   entry=entry->next;
+               }
+               return entry;
+           }
+       }
         return NULL;
     }
-};*/
-
+};
+//TC: aprox. O(n) [if fast pointer takes more turns in cycle otherwise traversing n nodes]
+//SC: O(1)
+/*----------------------------------------------------------------------------------------
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
@@ -51,3 +67,4 @@ public:
 };
 //TC: O(n) [traverse through list]
 //SC: O(n) [hashMap to store all n ele]
+*/
