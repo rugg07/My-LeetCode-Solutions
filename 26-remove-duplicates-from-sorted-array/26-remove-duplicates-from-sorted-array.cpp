3a16b8cfe560d0e513bd35f15737d12dd767b1aa
rugg07 <86529890+rugg07@users.noreply.github.com>
2022-02-05 15:52:13 +0400

    Time: 21 ms (32.66%), Space: 18.3 MB (74.81%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -2,6 +2,32 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         
+        //OPTIMAL: 2-pointer approach
+        
+        int count=0;
+        
+        for(int i=1; i<nums.size(); i++) //start from 2nd element (1st index)
+        {
+            if(nums[i]==nums[i-1]) //if current index element = previous index element
+                count++; //increase count for the element
+            
+            else
+                nums[i-count]=nums[i]; //otherwise add nums[i] value to nums[i-count]
+        }
+       
+        return nums.size()-count; //return the the first k values since it goes from 0 to nums.size()-count 
+    }
+};
+
+//why nums[i-count]? because since its sorted all duplicates will remain together so after going through one element it's count increases.Therefore, i-count gives the next index where no duplicate could be possible 
+
+//0,0,1,1 =>0,1
+
+/*----------------------------------------------------------------------------------------
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
         //BRUTE FORCE: using hashSet
         
         set<int> result;
@@ -21,28 +47,4 @@ public:
 };
 //TC: O(n) + O(n) = O(2n) = O(n) [store values in hashSet + store in nums arr again]
 //SC: O(n) [hashSet to store n ele]
-/*
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-        int count=0;
-        for(int i=1; i<nums.size(); i++) //start from 2nd element (1st index)
-        {
-            if(nums[i]==nums[i-1]) //if current index element = previous index element
-            {
-                count++; //increase count for the element
-            }
-            else
-            {
-                nums[i-count]=nums[i]; //otherwise add nums[i] value to nums[i-count]
-            }
-        }
-       
-        return nums.size()-count; //return the the first k values since it goes from 0 to nums.size()-count 
-    }
-};
-
-//why nums[i-count]? because since its sorted all duplicates will remain together so after going through one element it's count increases.Therefore, i-count gives the next index where no duplicate could be possible 
-
-//0,0,1,1 =>0,1
 */
