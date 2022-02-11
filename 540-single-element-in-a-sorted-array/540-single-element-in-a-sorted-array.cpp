588bf76e019e0fa064a7469c88ca225de8555d9a
rugg07 <86529890+rugg07@users.noreply.github.com>
2022-02-11 16:14:44 +0400

    Time: 31 ms (56.97%), Space: 22.5 MB (9.21%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-       
+        //OPTIMAL:  
+        int low = 0, high = nums.size() - 2; 
+        while(low <= high) {
+            int mid = (low + high)/2; 
+            if(nums[mid] == nums[mid^1]) {
+                low = mid + 1; 
+            } 
+            else {
+                high = mid - 1; 
+            }
+        }
+        return nums[low]; 
+    }
+};
+/*
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
         //BRUTE FORCE: xor ^ 
-        
         
         int res=0;
         //xor of values repeated twice is 0, the value stored is of the value present once
@@ -15,3 +31,4 @@ public:
 };
 //TC: O(n) [n ele]
 //SC: O(1)
+*/
