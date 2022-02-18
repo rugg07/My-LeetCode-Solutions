29dbc62e884edfb3bae097fb85d76ec509d89730
rugg07 <86529890+rugg07@users.noreply.github.com>
2022-02-18 16:09:35 +0400

    Time: 7 ms (85.57%), Space: 14.1 MB (58.94%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -2,6 +2,23 @@ class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
         
+      int ans=0;
+      for(int i=0;i<operations.size();i++)
+         {
+            if(operations[i]== "X++" || operations[i]== "++X" )
+                ans++;
+              
+            else //if it is "X--" or "--X"
+                ans--;
+         }
+      return ans;
+    }
+};
+/*----------------------------------------------------------------------------------------
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        
         int result=0; //final value being returned
         
         for(int i=0; i<operations.size(); i++)
@@ -20,3 +37,4 @@ public:
 };
 //TC: O(n) [traverse arr]
 //SC: O(1) 
+*/
