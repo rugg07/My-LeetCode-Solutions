b31a6056198d4d90b1636d7415122fe1f4313e32
rugg07 <86529890+rugg07@users.noreply.github.com>
2022-02-19 12:05:57 +0400

    Time: 21 ms (28.87%), Space: 9.8 MB (76.05%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -2,6 +2,31 @@ class Solution {
 public:
     int mostWordsFound(vector<string>& sentences) {
         
+        //Using count() STL
+        
+        int maxCount=0;
+        
+        //traverse to the string index
+        for(int i=0; i<sentences.size(); i++)
+        {
+            //keep track of count for each sentence going from its beg. to the end
+            //if we hit ' ' it means a word ended so we increase count by 1
+            int sCount = count(sentences[i].begin(), sentences[i].end(), ' ');
+            maxCount=max(maxCount, sCount+1); //sCount starts from 0 so we do sCount+1
+        }
+        return maxCount;
+    }
+};
+//TC: O(n*m) [traverse array string * for each string count() takes O(m) TC]
+//SC: O(1)
+
+/*---------------------------------------------------------------------------------------
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        
+        //Brute Force: without STL
+        
         int maxCount=0;
         
         //traverse to the string index
@@ -22,3 +47,4 @@ public:
 };
 //TC: O(n*m) [traverse array string * traverse words in each string/sentence]
 //SC: O(1)
+*/
