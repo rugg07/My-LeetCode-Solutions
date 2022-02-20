463a9e72788bb372398f031d88b5e066223a59f6
rugg07 <86529890+rugg07@users.noreply.github.com>
2022-02-20 17:29:45 +0400

    Time: 13 ms (17.81%), Space: 9.8 MB (58.95%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
+        
+        vector<int> result;
+        
+        //traverse from i = 0 to n=arr.size()/2 [0...n] and push values into arr
+        for(int i=0; i < n; i++)
+        {
+            result.push_back(nums[i]);
+            result.push_back(nums[n+i]);
+        }
+        return result;
+    }
+};
+//TC: O(n) [traverse arr]
+//SC: O(1) [result array is being returned so not counted]
+/*
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> result;
         
         //since n will divide the array into 2 equal halfs.
@@ -18,3 +36,4 @@ public:
 //SC: O(1) [result array is being returned so not counted]
 
 //we do i+n so that we get y1,y2,y3.. i=0 & n=3 in arr.size()=6 we put arr[0] and then 0+3=arr[3] goes into the arr.
+*/
