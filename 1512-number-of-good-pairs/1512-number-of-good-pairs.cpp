fb5602236e2324b23f09eadf7413349692f0fd1a
rugg07 <86529890+rugg07@users.noreply.github.com>
2022-02-23 01:22:46 +0400

    Time: 3 ms (54.44%), Space: 7.3 MB (16.05%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -1,8 +1,30 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        //BRUTE FORCE
         
+        //OPTIMAL: hashMap
+        int count=0;
+        unordered_map<int,int>result;
+
+        for (int i=0; i<nums.size(); i++) 
+        {
+            //if ele exists already it the hash stored for it gets added to count
+            count += result[nums[i]];
+            //we update the hash Value for that ele in hashMap
+            result[nums[i]]++;
+        }
+        return count;
+    }
+};
+//TC: O(n) [traverse arr]
+//SC: O(n) [hashMap]
+
+/*---------------------------------------------------------------------------------------
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        
+        //BRUTE FORCE
         int count=0;
         
         for(int i=0; i<nums.size();i++)
@@ -18,3 +40,4 @@ public:
 };
 //TC: O(n^2) [traversing array for each ele from i and j=i+1]
 //SC: O(1)
+*/
