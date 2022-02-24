9f56c44bdea6fe3276682578dc5e113bf9c8c515
rugg07 <86529890+rugg07@users.noreply.github.com>
2022-02-24 18:43:39 +0400

    Time: 11 ms (62.86%), Space: 15.2 MB (78.34%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -1,6 +1,37 @@
+
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
+        
+        //OPTIMAL: swapping (no extra space)
+        
+        for(int i=0; i<indices.size(); )
+        {
+            //if value same then no need to change charecter pos in string s
+            if(indices[i]==i) 
+                i++;
+            
+            //first swap the pos. of charecters for [i] and [indices[i]]
+            //then swap btw indices[i] & indices[indices[i]] 
+            //to put indices[i] item in its proper location in indices arr.
+            else 
+            {
+                swap(s[i],s[indices[i]]);
+                swap(indices[i],indices[indices[i]]);
+            }
+        }
+        return s;
+    }
+};
+//TC: O(n) [traverse indices and string s together]
+//SC: O(1) [no extra space]
+
+/*----------------------------------------------------------------------------------------
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        
+        //BRUTE FORCE: using extra space
         
         string result = s; //copy original string
         for(int i =0; i < indices.size(); i++)
@@ -13,3 +44,4 @@ public:
 };
 //TC: O(n)+O(n)=O(2n)=>O(n) [travere string + traverse arr]
 //SC: O(n) [for string but since string is returned it would be O(1)]
+*/
